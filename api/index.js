eb49916885786da3d6388ccf9e9eaa9a8a50adb7
Edinson <edisonprise@hotmail.com>
2022-12-03 17:09:47 -0500

    Se agrega el Proyecto

---
+++ api/index.js
@@ -0,0 +1,37 @@
+//                       _oo0oo_
+//                      o8888888o
+//                      88" . "88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \\|     |// '.
+//                 / \\|||  :  |||// \
+//                / _||||| -:- |||||- \
+//               |   | \\\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          ."" '<  `.___\_<|>_/___.' >' "".
+//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//         \  \ `_.   \_ __\ /__ _/   .-` /  /
+//     =====`-.____`.___ \_____/___.-`___.-'=====
+//                       `=---='
+//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const axios = require('axios');
+const Country = require('./src/models/Country.js');
+
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+  server.listen(3001, async() => {
+    const allCountries = Country.findAll();
+    if(!allCountries.length){
+      const apiCountriesResponse = await axios.get("https://restcountries.com/v3/all");
+      var apiCountries = apiCountriesResponse.data.map((e) =>{
+        
+      })
+    }
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  });
+});
